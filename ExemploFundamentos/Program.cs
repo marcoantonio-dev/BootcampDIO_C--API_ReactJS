ffd94b7edaf8d2c7fdc61c825c82d75457e573d9
marcoantonio-dev <maarcoantoniomasson@gmail.com>
2024-09-06 09:46:01 -0300

    ExemploFundamentos - by Leonardo Buta

---
+++ ExemploFundamentos/Program.cs
@@ -0,0 +1,27 @@
+﻿using ExemploFundamentos.Models;
+
+    DateTime dataAtual = DateTime.Now.AddMinutes(1);
+
+    Console.WriteLine($"Data e hora atual: {dataAtual}");
+
+
+// string apresentacao = "Olá, seja bem vindo";
+// int quantidade = 1;
+// double altura = 1.90;
+// decimal preco = 1.80M;
+// bool condicao = true;
+
+// Console.WriteLine(apresentacao);
+// Console.WriteLine("Valor da variável quantidade: " + quantidade);
+// Console.WriteLine("Valor da variável altura: " + altura);
+// Console.WriteLine("Valor da variável preco: " + preco);
+// Console.WriteLine("Valor da variável condicao: " + condicao);
+
+
+
+// /*
+// Pessoa pessoaUm = new Pessoa();
+
+// pessoaUm.Nome = "Marco Antônio";
+// pessoaUm.Idade = 18;
+// pessoaUm.Apresentar();*/
