31fef84d051d77a271504ad91548accd3b8123e0
marcoantonio-dev <maarcoantoniomasson@gmail.com>
2024-09-25 09:31:46 -0300

    :sparkles: feat: Trabalhando com operadores aritméticos

--- ExemploFundamentos/Program.cs
+++ ExemploFundamentos/Program.cs
@@ -14,38 +14,38 @@ Console.WriteLine(numero);
 Calculadora calcular = new Calculadora();
 calcular.RaizQuadrada(9);
 
-// calcular.Somar(10, 2);
-// calcular.Subtrair(10, 2);
-// calcular.Multiplicar(10, 2);
-// calcular.Dividir(10, 2);
-// calcular.Potencia(3,3);
-// calcular.Seno(30);
-// calcular.Coseno(30);
-// calcular.Tangente(30);
+ calcular.Somar(10, 2);
+ calcular.Subtrair(10, 2);
+ calcular.Multiplicar(10, 2);
+ calcular.Dividir(10, 2);
+ calcular.Potencia(3,3);
+ calcular.Seno(30);
+ calcular.Coseno(30);
+ calcular.Tangente(30);
 
 
-// DateTime dataAtual = DateTime.Now.AddMinutes(1);
+DateTime dataAtual = DateTime.Now.AddMinutes(1);
 
-// Console.WriteLine($"Data e hora atual: {dataAtual}");
-
-
-// string apresentacao = "Olá, seja bem vindo";
-// int quantidade = 1;
-// double altura = 1.90;
-// decimal preco = 1.80M;
-// bool condicao = true;
-
-// Console.WriteLine(apresentacao);
-// Console.WriteLine("Valor da variável quantidade: " + quantidade);
-// Console.WriteLine("Valor da variável altura: " + altura);
-// Console.WriteLine("Valor da variável preco: " + preco);
-// Console.WriteLine("Valor da variável condicao: " + condicao);
+ Console.WriteLine($"Data e hora atual: {dataAtual}");
 
 
+ string apresentacao = "Olá, seja bem vindo";
+ int quantidade = 1;
+ double altura = 1.90;
+ decimal preco = 1.80M;
+ bool condicao = true;
 
-// /*
-// Pessoa pessoaUm = new Pessoa();
+ Console.WriteLine(apresentacao);
+ Console.WriteLine("Valor da variável quantidade: " + quantidade);
+ Console.WriteLine("Valor da variável altura: " + altura);
+ Console.WriteLine("Valor da variável preco: " + preco);
+ Console.WriteLine("Valor da variável condicao: " + condicao);
 
-// pessoaUm.Nome = "Marco Antônio";
-// pessoaUm.Idade = 18;
-// pessoaUm.Apresentar();*/
+
+
+
+ Pessoa pessoaUm = new Pessoa();
+
+ pessoaUm.Nome = "Marco Antônio";
+ pessoaUm.Idade = 18;
+ pessoaUm.Apresentar();
